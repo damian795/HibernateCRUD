e50c664624bd420f4773249d9cb37a3b3d9f1e6a
Damian Spodniewski <35959738+damian795@users.noreply.github.com>
2024-10-23 22:17:28 +0200

    Add files via upload

---
+++ GamesCRUD/src/main/java/org/example/Game.java
@@ -0,0 +1,70 @@
+package org.example;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "games")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private LocalDate releaseDate;
+
+    private String platform;
+
+    public Game() {}
+
+    public Game(String name, LocalDate releaseDate, String platform) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.platform = platform;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", releaseDate=" + releaseDate +
+                ", platform='" + platform + '\'' +
+                '}';
+    }
+}
---
+++ GamesCRUD/src/main/java/org/example/GameCRUDTest.java
@@ -0,0 +1,73 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class GameCRUDTest {
+
+    private SessionFactory sessionFactory;
+    private Session session;
+    private GameCRUD gameCRUD;
+
+    @Before
+    public void setUp() {
+        //Użycie osobnej konfiguracji testowej
+        sessionFactory = new Configuration().configure("hibernate-test.cfg.xml").buildSessionFactory();
+        session = sessionFactory.openSession();
+
+        gameCRUD = new GameCRUD();
+    }
+
+    @After
+    public void tearDown() {
+        session.close();
+        sessionFactory.close();
+    }
+
+    @Test
+    public void testSaveGame() {
+        Game game = new Game("Cyberpunk 2077", LocalDate.of(2020, 12, 10), "PC");
+        gameCRUD.saveGame(game);
+
+        List<Game> games = gameCRUD.getAllGames();
+        //Sprawdza, czy rozmiar listy gier wynosi 1 co oznacza że gra została poprawnie dodana
+        assertEquals(1, games.size());
+        //Sprawdza, czy nazwa pierwszej gry w liście to "Cyberpunk 2077"
+        //asercja sprawdza czy wyniki sa zgodne z oczekiwaniami
+        assertEquals("Cyberpunk 2077", games.get(0).getName());
+    }
+
+    @Test
+    public void testUpdateGame() {
+        //Tworzenie nowej gry i zapisywanie jej
+        Game game = new Game("Cyberpunk 2077", LocalDate.of(2020, 12, 10), "PC");
+        Integer id = gameCRUD.saveGame(game);  // Zakładam, że saveGame zwraca ID nowej gry
+
+        //Aktualizacja gry
+        gameCRUD.updateGame(15, "Cyberpunk 2077 Updated");
+
+        //Pobranie zaktualizowanej gry
+        Game updatedGame = gameCRUD.getGame(id);
+        assertEquals("Cyberpunk 2077 Updated", updatedGame.getName());
+    }
+
+    @Test
+    public void testDeleteGame() {
+        Game game = new Game("Cyberpunk 2077", LocalDate.of(2020, 12, 10), "PC");
+        gameCRUD.saveGame(game);
+
+        gameCRUD.deleteGame(game.getId());
+
+        List<Game> games = gameCRUD.getAllGames();
+        assertEquals(0, games.size());
+    }
+}
